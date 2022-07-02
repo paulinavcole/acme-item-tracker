053940fea08d302c138b2bc148531a04b3a6a02d
Paulina Cole <lina.cole@gmail.com>
2022-07-02 14:35:06 -0400

    updated store to reflect deleting thing, creating user, deleting user

--- src/store.js
+++ src/store.js
@@ -19,6 +19,18 @@ const store = createStore((state = initialState, action)=> {
   if(action.type === 'CREATE_THING'){
     return {...state, things: [...state.things, action.thing ]}; 
   }
+  if(action.type === 'DELETE_THING'){
+    const things = state.things.filter(_thing => _thing.id !== action.thing.id);
+    return {...state, things}; 
+  }
+  if(action.type === 'CREATE_USER'){
+    return {...state, users: [...state.users, action.user]};
+  }
+  if(action.type === 'DELETE_USER'){
+    const users = state.users.filter(_user => _user.id !== action.user.id);
+    const things = state.things.filter(_thing => _thing.userId !== action.user.id);
+    return {...state, users, things};
+  }
   return state;
 });
 
